af77d92c8895174432d4a43b92b88947e8249c05
discretegames <discretizedgames@gmail.com>
2021-06-29 21:55:35 -0700

    Wrote init and setup pys.

---
+++ mathdunders/__init__.py
@@ -0,0 +1,3 @@
+from mathdunders.mathdunders import binary
+from mathdunders.mathdunders import unary
+from mathdunders.mathdunders import mathdunders
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from os import read
+
+VERSION = "0.1"
+
+setup(
+    name='mathdunders',
+    version=VERSION,
+    author='discretegames',
+    author_email='discretizedgames@gmail.com',
+    description="Decorator that adds math dunders to a class derived from a numeric type.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/discretegames/mathdunders',
+    packages=['mathdunders'],
+    license="MIT License"
+)
